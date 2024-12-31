84af38d9a31632d547e527ef06dd3123f5b3c0cd
rucas <lucas.rondenet@gmail.com>
2024-12-31 09:48:26 -0800

    clean up nix warnings

--- flake.nix
+++ flake.nix
@@ -40,11 +40,11 @@
       flake = false;
     };
   };
-  outputs = { nixpkgs, nixvim, flake-parts, pre-commit-hooks, ... }@inputs:
+  outputs = { nixvim, flake-parts, pre-commit-hooks, ... }@inputs:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems =
         [ "aarch64-linux" "x86_64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { system, pkgs, self', lib, ... }:
+      perSystem = { system, pkgs, self', ... }:
         let
           nixvim' = nixvim.legacyPackages.${system};
           nvim = nixvim'.makeNixvimWithModule {
