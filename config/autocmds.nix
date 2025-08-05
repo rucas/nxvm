784291544c2f8e61fffa184d2dafe410e67e30fb
rucas <lucas.rondenet@gmail.com>
2025-08-05 08:54:29 -0700

    follow cursor cursorline

--- config/autocmds.nix
+++ config/autocmds.nix
@@ -8,5 +8,15 @@
       command = "checktime";
       desc = "Check for external file changes when gaining focus or entering buffer";
     }
+    {
+      event = "BufLeave";
+      command = "set nocursorline";
+      desc = "Disable cursor line highlight when leaving a buffer";
+    }
+    {
+      event = "BufEnter";
+      command = "set cursorline";
+      desc = "Enable cursor line highlight when entering a buffer";
+    }
   ];
 }
