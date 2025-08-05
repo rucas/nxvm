eff0997e9aa16f564af876572432d23fddcb9e9a
rucas <lucas.rondenet@gmail.com>
2025-08-05 10:29:52 -0700

    quicker size respect neo-tree

--- config/plug/ui/quicker.nix
+++ config/plug/ui/quicker.nix
@@ -1,5 +1,24 @@
 {
   plugins.quicker = {
     enable = true;
+    settings = {
+      on_qf = {
+        __raw = ''
+          function(bufnr)
+            local name = vim.fn.bufname("neo-tree")
+            local winnr = vim.fn.bufwinnr(name)
+
+            if winnr == 1 then
+              vim.defer_fn(function()
+                local cmd = string.format("Neotree toggle")
+                vim.cmd(cmd)
+                vim.cmd(cmd)
+                vim.cmd("wincmd p")
+              end, 100)
+            end
+          end
+        '';
+      };
+    };
   };
 }
