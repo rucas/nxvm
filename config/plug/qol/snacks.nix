4849de46fc55ff37642aa008adda412611301a60
rucas <lucas.rondenet@gmail.com>
2025-08-08 16:38:16 -0700

    no indent for norg

--- config/plug/qol/snacks.nix
+++ config/plug/qol/snacks.nix
@@ -12,6 +12,18 @@
         animate = {
           enabled = false;
         };
+        filter.__raw = ''
+          function(buf)
+            local filetype = vim.bo[buf].filetype
+            -- Exclude norg files
+            if filetype == "norg" then
+              return false
+            end
+            return vim.g.snacks_indent ~= false 
+              and vim.b[buf].snacks_indent ~= false 
+              and vim.bo[buf].buftype == ""
+          end
+        '';
       };
     };
   };
