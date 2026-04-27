15da864e25b9897259d2369f20180852bfa49c27
rucas <lucas.rondenet@gmail.com>
2026-04-27 10:53:06 -0700

    fix nix flake check

--- pkgs/tree-sitter-tera.nix
+++ pkgs/tree-sitter-tera.nix
@@ -1,5 +1,6 @@
 { inputs, tree-sitter }:
 tree-sitter.buildGrammar {
+  pname = "tree-sitter-tera";
   language = "tera";
   version = inputs.tree-sitter-tera.shortRev;
   src = inputs.tree-sitter-tera;
